abfd8b458ac5dd1676d48ab53f6f0b9304324bc0
WashiBH <washi1886@gmail.com>
2017-11-29 11:06:24 -0500

    Agregacion de Procesos

--- src/com/pricing/model/CProceso.java
+++ src/com/pricing/model/CProceso.java
@@ -1,5 +1,24 @@
 package com.pricing.model;
 
 public class CProceso {
-	private int ncodProceso;
+	private int nroproceso;
+	private String descripcion;
+	//get and set
+	public int getNroproceso() {
+		return nroproceso;
+	}
+	public void setNroproceso(int nroproceso) {
+		this.nroproceso = nroproceso;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	// procesos
+	public CProceso(){
+		nroproceso=1;
+		descripcion="";
+	}
 }
